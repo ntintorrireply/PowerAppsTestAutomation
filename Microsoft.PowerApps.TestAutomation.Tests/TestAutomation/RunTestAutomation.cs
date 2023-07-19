bc2905dacecd2ca91597ce9859583ae3e4411b4f
Nicola Tintorri <n.tintorri@reply.it>
2023-07-19 17:29:42 +0200

    PrivateMode

--- Microsoft.PowerApps.TestAutomation.Tests/TestAutomation/RunTestAutomation.cs
+++ Microsoft.PowerApps.TestAutomation.Tests/TestAutomation/RunTestAutomation.cs
@@ -59,7 +59,7 @@ namespace Microsoft.PowerApps.TestAutomation.Tests
             BrowserOptions options = RunTestSettings.Options;
             options.BrowserType = _browserType;
             options.DriversPath = _driversPath;
-            options.UsePrivateMode = _usePrivateMode;
+            options.PrivateMode = _usePrivateMode;
 
             using (var appBrowser = new PowerAppBrowser(options))
             {
